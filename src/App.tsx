b67c28b6abf96202aa4dcdd58faa1bb44713ccc8
Dmitry <d2013.dima@mail.ru>
2024-09-11 22:22:17 +0500

    Filter method

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 import { tasksPropsType, Todolist } from "./components/todolist/Todolist";
+import { Button } from "./components/button/Button";
 
 const todolists = [
   {
@@ -22,6 +23,36 @@ const todolists = [
 
 export type FilterValuesType = "all" | "completed" | "active";
 function App() {
+  let [money, setMoney] = useState([
+    { banknots: "Dollars", value: 100, number: " a1234567890" },
+    { banknots: "Dollars", value: 50, number: " z1234567890" },
+    { banknots: "RUBLS", value: 100, number: " w1234567890" },
+    { banknots: "Dollars", value: 100, number: " e1234567890" },
+    { banknots: "Dollars", value: 50, number: " c1234567890" },
+    { banknots: "RUBLS", value: 100, number: " r1234567890" },
+    { banknots: "Dollars", value: 50, number: " x1234567890" },
+    { banknots: "RUBLS", value: 50, number: " v1234567890" },
+  ]);
+
+  let [nameBtn, setnameBtn] = useState("All");
+
+  let currentMoney = money;
+
+  if (nameBtn === "RUBLS") {
+    currentMoney = money.filter(
+      (filteredMoney) => filteredMoney.banknots === "RUBLS"
+    );
+  }
+  if (nameBtn === "Dollars") {
+    currentMoney = money.filter(
+      (filteredMoney) => filteredMoney.banknots === "Dollars"
+    );
+  }
+
+  const onClickFilterHandler = (nameButton: string) => {
+    setnameBtn(nameButton);
+  };
+
   //   let initTasks2: Array<tasksPropsType> = [
   //     {
   //       id: 1,
@@ -76,6 +107,33 @@ function App() {
         tasks={initTasks2}
         removeTask={removeTask}
       /> */}
+      <ul>
+        {currentMoney.map((e) => {
+          return (
+            <li key={e.number}>
+              banknots: {e.banknots} nominal: {e.value} seria: {e.number}
+            </li>
+          );
+        })}
+      </ul>
+      <Button
+        name="All"
+        callBack={() => {
+          onClickFilterHandler("All");
+        }}
+      />
+      <Button
+        name="Dollars"
+        callBack={() => {
+          onClickFilterHandler("Dollars");
+        }}
+      />
+      <Button
+        name="RUBLS"
+        callBack={() => {
+          onClickFilterHandler("RUBLS");
+        }}
+      />
     </div>
   );
 }
