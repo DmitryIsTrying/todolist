1c2516e036e1ac7afa231bad4e82d555c830299c
Dmitry <d2013.dima@mail.ru>
2024-09-20 00:59:48 +0500

    useState learn, const useState not let

--- src/App.tsx
+++ src/App.tsx
@@ -1,92 +1,97 @@
-import React, {useState} from "react";
-import "./App.css";
-import {tasksPropsType, Todolist} from "./components/todolist/Todolist";
-import {v1} from "uuid";
-import {Network} from "./netwrorking";
+import React, { useState } from 'react'
+import './App.css'
+import { tasksPropsType, Todolist as Todo } from './components/todolist/Todolist'
+import { v1 } from 'uuid'
+import { Network } from './netwrorking'
 
 const test = Network
 
-
 const todolist = [
-    {
-        id: v1(),
-        title: "JS",
-        isDone: true,
-    },
-    {
-        id: v1(),
-        title: "HTML&CSS",
-        isDone: true,
-    },
-    {
-        id: v1(),
-        title: "React",
-        isDone: false,
-    },
-];
+  {
+    id: '1',
+    title: 'JS',
+    isDone: true,
+  },
+  {
+    id: '2',
+    title: 'HTML&CSS',
+    isDone: true,
+  },
+  {
+    id: '3',
+    title: 'React',
+    isDone: false,
+  },
+]
 
-
-export type FilterValuesType = "all" | "completed" | "active";
+export type FilterValuesType = 'all' | 'completed' | 'active'
 
 function App() {
+  const [tasks, setTasks] = useState<Array<tasksPropsType>>(todolist)
 
-    let [tasks, setTasks] = useState<Array<tasksPropsType>>(todolist);
+  const [filter, setFilter] = useState<FilterValuesType>('all')
 
-    let [filter, setFilter] = useState<FilterValuesType>("all");
+  function removeTask(id: string) {
+    setTasks([...tasks.filter((e) => e.id !== id)])
+  }
 
-    function removeTask(id: string) {
-        tasks = tasks.filter((e) => e.id !== id);
-        setTasks(tasks);
+  function addTask(taskTitle: string) {
+    const noSpaceTaskTitle = taskTitle.trim()
+    if (noSpaceTaskTitle.length > 0) {
+      let newTask = { id: v1(), title: noSpaceTaskTitle, isDone: false }
+      let newTasks = [newTask, ...tasks]
+      setTasks(newTasks)
     }
+  }
 
-    function addTask(taskTitle: string) {
-        const noSpaceTaskTitle = taskTitle.trim()
-        if (noSpaceTaskTitle.length > 0) {
-            let newTask = {id: v1(), title: noSpaceTaskTitle, isDone: false};
-            let newTasks = [newTask, ...tasks];
-            setTasks(newTasks);
-        }
-    }
+  function changeFilter(value: FilterValuesType) {
+    setFilter(value)
+  }
 
-    function changeFilter(value: FilterValuesType) {
-        setFilter(value);
-    }
+  let tasksForTodolist = tasks
+  if (filter === 'completed') {
+    tasksForTodolist = tasks.filter((t) => t.isDone)
+  }
+  if (filter === 'active') {
+    tasksForTodolist = tasks.filter((t) => !t.isDone)
+  }
 
-    let tasksForTodolist = tasks;
-    if (filter === "completed") {
-        tasksForTodolist = tasks.filter((t) => t.isDone);
-    }
-    if (filter === "active") {
-        tasksForTodolist = tasks.filter((t) => !t.isDone);
-    }
+  const [index, setIndex] = useState(0)
 
-    return (
-        <div className="App">
-            <Todolist
-                date="10.09.2024"
-                title="What to learn"
-                tasks={tasksForTodolist}
-                removeTask={removeTask}
-                changeFilter={changeFilter}
-                addTask={addTask}
-            />
-            {/*<Todolists/>*/}
-            {/*<Todolist*/}
-            {/*    title="What to learn"*/}
-            {/*    tasks={tasksForTodolist}*/}
-            {/*    removeTask={removeTask}*/}
-            {/*    changeFilter={changeFilter}*/}
-            {/*    addTask={addTask}*/}
-            {/*/>*/}
-            {/*<Todolist*/}
-            {/*    title="What to read"*/}
-            {/*    tasks={tasksForTodolist}*/}
-            {/*    removeTask={removeTask}*/}
-            {/*    changeFilter={changeFilter}*/}
-            {/*    addTask={addTask}*/}
-            {/*/>*/}
-        </div>
-    );
+  function upIndex() {
+    setIndex(index >= 5 ? 0 : index + 1)
+  }
+
+  return (
+    <div className="App">
+      <Todo
+        date="10.09.2024"
+        title="What to learn"
+        tasks={tasksForTodolist}
+        removeTask={removeTask}
+        changeFilter={changeFilter}
+        addTask={addTask}
+      />
+      <button style={{ height: '300px', width: '300px' }} onClick={upIndex}>
+        {index}
+      </button>
+      {/*<Todolists/>*/}
+      {/*<Todolist*/}
+      {/*    title="What to learn"*/}
+      {/*    tasks={tasksForTodolist}*/}
+      {/*    removeTask={removeTask}*/}
+      {/*    changeFilter={changeFilter}*/}
+      {/*    addTask={addTask}*/}
+      {/*/>*/}
+      {/*<Todolist*/}
+      {/*    title="What to read"*/}
+      {/*    tasks={tasksForTodolist}*/}
+      {/*    removeTask={removeTask}*/}
+      {/*    changeFilter={changeFilter}*/}
+      {/*    addTask={addTask}*/}
+      {/*/>*/}
+    </div>
+  )
 }
 
-export default App;
+export default App
--- src/netwrorking.ts
+++ src/netwrorking.ts
@@ -1,7 +1,3 @@
 export const Network = 1
 
 let arr = ['t', 'e', 's', 't']
-
-let removed = arr.slice(0, 0)
-
-console.log(removed) // t
